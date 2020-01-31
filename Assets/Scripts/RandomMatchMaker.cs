24c4a707df64b3729c280f501a5cf481a479df0b
佐々木勇星 <y_sasaki@vrlab.esys.tsukuba.ac.jp>
2020-01-31 14:45:05 +0900

    Add : Photon Random Match Maker script

---
+++ Assets/Scripts/RandomMatchMaker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+public class RandomMatchMaker : Photon.PunBehaviour
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    //ロビーに入室した時
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        // ルームのどれかに入室する
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    // ルームのランダム入室に失敗
+    public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+    {
+        base.OnPhotonRandomJoinFailed(codeAndMsg);
+        Debug.Log("Cant join random room!");
+    }
+
+}
