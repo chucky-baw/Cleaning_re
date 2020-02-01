2c41619fe97bb5a5b9e5f17a2f6aae8e38038361
佐々木勇星 <y_sasaki@vrlab.esys.tsukuba.ac.jp>
2020-02-01 11:14:51 +0900

    Add :Player arrived
    入室した際に，他プレイヤー待機，到着等の処理をログで確認

--- Assets/Scripts/RandomMatchMaker.cs
+++ Assets/Scripts/RandomMatchMaker.cs
@@ -30,6 +30,29 @@ public class RandomMatchMaker : Photon.PunBehaviour
         PhotonNetwork.JoinRandomRoom();
     }
 
+    //部屋に入室した時
+    public override void OnJoinedRoom()
+    {
+        //playerが揃った時
+        //ゲームを開始すれば良い
+        if (PhotonNetwork.room.PlayerCount == 2)
+        {
+            Debug.Log("I'm second player!");
+        }
+        else
+        {
+            //もう一人のプレイヤーを待つ
+            Debug.Log("Waiting for another player");
+        }
+    }
+
+    //他プレイヤーが到着した時
+    public override void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
+    {
+        Debug.Log("Other player arrived");
+    }
+
+
     // ルームのランダム入室に失敗
     public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
     {
